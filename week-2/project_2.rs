f44da4c5cbf145afc5f0b4616c5aeee01edff3d8
Farid Aderogba <farid.aderogba@pau.edu.ng>
2025-10-11 16:50:36 +0100

    (My first commit)

--- week-2/project_2.rs
+++ week-2/project_2.rs
@@ -1,3 +1,15 @@
-fn fn main() {
-	let
+fn main() {
+	let Toshiba: = 450000.00
+	let Mac: = 1500000.00
+	let HP: = 750000.00
+	let Dell: = 2850000.00
+	let Acer: = 250000.00
+
+	// Sum
+	let S = (Toshiba + Mac + HP + Dell + Acer);
+	println!("Sum is {}", S);
+
+	// Average 
+	let A = S / 5;
+	println!("Average is {}", A );
 }
