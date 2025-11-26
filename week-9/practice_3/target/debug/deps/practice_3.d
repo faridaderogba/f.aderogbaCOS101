3e8d92f901fb0b9a314c3b14e0bac12627f8db94
Farid Aderogba <farid.aderogba@pau.edu.ng>
2025-11-26 13:43:37 +0100

    Input and Output in rust

---
+++ week-9/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\Ademide\OneDrive\Documents\PAU\Data Science\Year 1\Semester 1\f.aderogbaCOS101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\Ademide\OneDrive\Documents\PAU\Data Science\Year 1\Semester 1\f.aderogbaCOS101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
