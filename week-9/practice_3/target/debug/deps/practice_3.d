9d52ea78c7203832696f1335e4e39103d642759d
Farid Aderogba <farid.aderogba@pau.edu.ng>
2025-11-26 13:47:50 +0100

    Input and Output

--- week-9/practice_3/target/debug/deps/practice_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ademide\OneDrive\Documents\PAU\Data Science\Year 1\Semester 1\f.aderogbaCOS101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
-
-C:\Users\Ademide\OneDrive\Documents\PAU\Data Science\Year 1\Semester 1\f.aderogbaCOS101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
-
-src\main.rs:
