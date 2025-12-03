ed8219b0b13b6c287b503a8c0f10f7abaac898e9
Farid Aderogba <farid.aderogba@pau.edu.ng>
2025-12-03 13:48:46 +0100

    Using method in struct

--- week-10/practice_9/src/main.rs
+++ week-10/practice_9/src/main.rs
@@ -1,3 +1,29 @@
+// define dimensions of a rectangle
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// logic to calculate area of a rectangle
+impl Rectangle {
+    fn area(&self) -> u32 {
+        // use the . operator to fetch the value of a field via the self keyword
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // instantiate the structure
+    let small = Rectangle {
+        width: 10,
+        height: 20,
+    };
+
+    // print the rectangle's area
+    println!(
+        "width is {} \nheight is {} \narea of Rectangle is {}",
+        small.width,
+        small.height,
+        small.area()
+    );
 }
