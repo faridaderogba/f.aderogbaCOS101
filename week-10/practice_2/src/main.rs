0a2193c3692044e779cbd7f517f483b1e6909d0e
Farid Aderogba <farid.aderogba@pau.edu.ng>
2025-12-03 13:19:03 +0100

    passing value to a funciton

--- week-10/practice_2/src/main.rs
+++ week-10/practice_2/src/main.rs
@@ -1,11 +1,18 @@
 fn main() {
     let v = vec![10,20,30];
-    let v2 = v;
-    display(v2);
+    //vector v owns the objecct in heap
+
+    let v2=v; //moves ownership to v2
+    display(v2.clone());
     //v2 is moved to display and v2 is invalidated
 
-    
     println!("In main {:?}",v2);
-
-
+    //v2 is no longer usable here
 }
+fn display(v:Vec<i32>){
+    println!("Inside display {:?}",v)
+}
+
+
+
+
