3c45f1277caab7a14a8b10920c1775891e12e635
Farid Aderogba <farid.aderogba@pau.edu.ng>
2025-12-03 13:41:01 +0100

    Practice 7

--- week-10/practice_7/src/main.rs
+++ week-10/practice_7/src/main.rs
@@ -1,3 +1,17 @@
+struct Employee {
+    name: String,
+    company: String,
+    age: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let emp1 = Employee {
+        company: String::from("Ernst & Young"),
+        name: String::from("Ebibiong Jessica"),
+        age: 25,
+    };
+
+    println!("Name = {} \n", emp1.name);
+    println!("Company = {} \n", emp1.company);
+    println!("Age = {}", emp1.age);
 }
